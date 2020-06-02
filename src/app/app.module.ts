e45bf7275cacd60816c6494dca11f3cd6e76db0c
maxi <maxiranqui@gmail.com>
2020-06-02 10:13:37 -0300

    Se modifica component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HospitalComponent } from './componentesalud/hospital/hospital.component';
+import { PacienteComponent } from './componentesalud/paciente/paciente.component';
+import { MedicoComponent } from './componentesalud/medico/medico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HospitalComponent,
+    PacienteComponent,
+    MedicoComponent
   ],
   imports: [
     BrowserModule,
